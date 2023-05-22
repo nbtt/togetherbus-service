c0431b44438c9c00c3271af95b7f1323851ab897
tuantnb <tuantnb@vng.com.vn>
2023-05-22 16:40:58 +0700

    update entity order add account ref

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,11 +1,13 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { OrderItem } from "./order-item";
+import { Account } from "./account.entity";
 
 @Entity()
 export class Order {
     @PrimaryGeneratedColumn()
     id: number;
     
+    @Index()
     @Column({type: "timestamp"})
     created_time: Date;
 
@@ -21,6 +23,10 @@ export class Order {
         item => item.order,
     )
     items: OrderItem[];
+
+    @Index()
+    @ManyToOne(type => Account)
+    account: Account;
 
     // get amount before subtracting discount
     getOriginalAmount = () => {
